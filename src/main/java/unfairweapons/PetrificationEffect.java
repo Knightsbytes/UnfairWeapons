5de58777824a18ad7414e18df02739f71aa6be4d
ben <benjaminburton2020@outlook.com>
2026-01-07 21:40:00 +0000

    Balancing changes :)

--- src/main/java/unfairweapons/PetrificationEffect.java
+++ src/main/java/unfairweapons/PetrificationEffect.java
@@ -4,12 +4,15 @@ import net.minecraft.resources.Identifier;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
+import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.player.Player;
 
+import static net.minecraft.world.effect.MobEffects.REGENERATION;
 import static unfairweapons.UnfairWeapons.MOD_ID;
+import static unfairweapons.UnfairWeapons.PETRIFICATION_EFFECT;
 
 public class PetrificationEffect extends MobEffect {
     protected PetrificationEffect() {
@@ -26,7 +29,14 @@ public class PetrificationEffect extends MobEffect {
     @Override
     public boolean applyEffectTick(ServerLevel world, LivingEntity entity, int amplifier) {
         if (entity instanceof Player) {
+            if (entity.isOnFire()){
+                entity.clearFire();
+            }
 
+            if (entity.getHealth() <= 4){
+                MobEffectInstance HealthRegen = new MobEffectInstance(REGENERATION, 2, 0);
+                entity.addEffect(HealthRegen);
+            }
         }
 
         return super.applyEffectTick(world, entity, amplifier);

3f325ccd8a6c40831e11f5ce795835f4fb505cdb
ben <benjaminburton2020@outlook.com>
2026-01-10 21:36:05 +0000

    Working on keybind functionality

--- src/client/java/unfairweapons/UnfairWeaponsClient.java
+++ src/client/java/unfairweapons/UnfairWeaponsClient.java
@@ -2,12 +2,16 @@ package unfairweapons;
 
 import com.mojang.blaze3d.platform.InputConstants;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.KeyMapping;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.Identifier;
+import net.minecraft.world.effect.MobEffectInstance;
 import org.lwjgl.glfw.GLFW;
 
 import static unfairweapons.UnfairWeapons.MOD_ID;
+import static unfairweapons.UnfairWeapons.PETRIFICATION_EFFECT;
 
 public class UnfairWeaponsClient implements ClientModInitializer {
 	KeyMapping.Category ELDRITCH_ABILITIES = new KeyMapping.Category(Identifier.fromNamespaceAndPath(MOD_ID, "eldritch_abilities"));
@@ -21,6 +25,54 @@ public class UnfairWeaponsClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+		ClientTickEvents.END_CLIENT_TICK.register(client -> {
+			while (PetrificationAbility1.consumeClick()) {
+				MobEffectInstance effectInstance = client.player.getEffect(PETRIFICATION_EFFECT);
+
+				if (effectInstance != null) {
+					if (client.player != null) {
+						if (effectInstance.getAmplifier() >= 0) {
+							client.player.displayClientMessage(Component.literal("Key pressed!"), false);
+
+						} else if (effectInstance.getAmplifier() < 0) {
+							client.player.displayClientMessage(Component.literal("You don't have the needed effect to use this!"), false);
+						}
+					}
+				}
+
+			}
+
+			while (PetrificationAbility2.consumeClick()) {
+				MobEffectInstance effectInstance = client.player.getEffect(PETRIFICATION_EFFECT);
+
+				if (effectInstance != null) {
+					if (client.player != null) {
+						if (effectInstance.getAmplifier() >= 1) {
+							client.player.displayClientMessage(Component.literal("Key pressed!"), false);
+
+						} else if (effectInstance.getAmplifier() < 1) {
+							client.player.displayClientMessage(Component.literal("You don't have the needed effect to use this!"), false);
+						}
+					}
+				}
+
+			}
+
+			while (PetrificationAbility3.consumeClick()) {
+				MobEffectInstance effectInstance = client.player.getEffect(PETRIFICATION_EFFECT);
+
+				if (effectInstance != null) {
+					if (client.player != null) {
+						if (effectInstance.getAmplifier() >= 1) {
+							client.player.displayClientMessage(Component.literal("Key pressed!"), false);
+
+						} else if (effectInstance.getAmplifier() < 1) {
+							client.player.displayClientMessage(Component.literal("You don't have the needed effect to use this!"), false);
+						}
+					}
+				}
+
+			}
+		});
 	}
 }

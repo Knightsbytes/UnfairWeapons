58d2cb9476c6dc7bbf082365aa99f32072bf9ec5
ben <benjaminburton2020@outlook.com>
2026-01-11 16:36:41 +0000

    Updated the abilities UI

--- src/client/java/unfairweapons/UnfairWeaponsClient.java
+++ src/client/java/unfairweapons/UnfairWeaponsClient.java
@@ -9,6 +9,7 @@ import net.fabricmc.fabric.api.client.rendering.v1.hud.VanillaHudElements;
 import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.renderer.RenderPipelines;
 import net.minecraft.network.chat.Component;
@@ -26,9 +27,20 @@ import static unfairweapons.UnfairWeapons.MOD_ID;
 import static unfairweapons.UnfairWeapons.PETRIFICATION_EFFECT;
 
 public class UnfairWeaponsClient implements ClientModInitializer {
+
     private static final Identifier PETRIFICATION_COOLDOWNS_BACKGROUND = Identifier.fromNamespaceAndPath(
             MOD_ID,
             "textures/gui/petrification_image_background.png"
+    );
+
+    private static final Identifier PETRIFICATION_GUI_NEEDLE_FULL = Identifier.fromNamespaceAndPath(
+            MOD_ID,
+            "textures/gui/petrification_gui_needle_full.png"
+    );
+
+    private static final Identifier PETRIFICATION_GUI_NEEDLE_EMPTY = Identifier.fromNamespaceAndPath(
+            MOD_ID,
+            "textures/gui/petrification_gui_needle_empty.png"
     );
 
 	KeyMapping.Category ELDRITCH_ABILITIES = new KeyMapping.Category(Identifier.fromNamespaceAndPath(MOD_ID, "eldritch_abilities"));
@@ -40,6 +52,10 @@ public class UnfairWeaponsClient implements ClientModInitializer {
 	public final KeyMapping PetrificationAbility3 = KeyBindingHelper.registerKeyBinding(new KeyMapping("key.ability.eldritch_ability_3", GLFW.GLFW_KEY_N, ELDRITCH_ABILITIES));
 
     private static final HashMap<UUID, Long> cooldowns = new HashMap<>();
+
+    private static long PetrificationCooldown1;
+    private static long PetrificationCooldown2;
+    private static long PetrificationCooldown3;
 
 	@Override
 	public void onInitializeClient() {
@@ -54,6 +70,25 @@ public class UnfairWeaponsClient implements ClientModInitializer {
 
             long currentTick = client.level.getGameTime();
             UUID playerId = client.player.getUUID();
+
+            String cooldownKey1 = playerId + "_ability1";
+            String cooldownKey2 = playerId + "_ability2";
+            String cooldownKey3 = playerId + "_ability3";
+
+            if (cooldowns.getOrDefault(cooldownKey1, 0L) > currentTick) {
+                long remaining1 = cooldowns.get(cooldownKey1) - currentTick;
+                PetrificationCooldown1 = remaining1 / 20;
+            }
+
+            if (cooldowns.getOrDefault(cooldownKey2, 0L) > currentTick) {
+                long remaining2 = cooldowns.get(cooldownKey2) - currentTick;
+                PetrificationCooldown2 = remaining2 / 20;
+            }
+
+            if (cooldowns.getOrDefault(cooldownKey3, 0L) > currentTick) {
+                long remaining3 = cooldowns.get(cooldownKey3) - currentTick;
+                PetrificationCooldown3 = remaining3 / 20;
+            }
 
             // Ability 1
             while (PetrificationAbility1.consumeClick()) {
@@ -163,9 +198,7 @@ public class UnfairWeaponsClient implements ClientModInitializer {
         int width = 128;
         int height = 32;
 
-        Identifier vanillaTest = Identifier.fromNamespaceAndPath("minecraft", "textures/gui/icons.png");
-        context.blit(vanillaTest, x, y, 0, 0, 128, 32, 256, 256);
-
+        Font font = mc.font;
 
         if (mc.player.hasEffect(PETRIFICATION_EFFECT)){
             //context.fill(x, y, x + width, y + height, 0xFFFF0000);
@@ -180,6 +213,30 @@ public class UnfairWeaponsClient implements ClientModInitializer {
                         width, height,
                         width, height
                 );
+
+                if (effectInstanceAmplification == 1) {
+
+                    context.blit(
+                            RenderPipelines.GUI_TEXTURED,
+                            PETRIFICATION_GUI_NEEDLE_FULL,
+                            x + 109, y + 9, 0, 0,
+                            13, 16,
+                            13, 16
+                    );
+                }
+                else{
+                    context.blit(
+                            RenderPipelines.GUI_TEXTURED,
+                            PETRIFICATION_GUI_NEEDLE_EMPTY,
+                            x + 109, y + 9, 0, 0,
+                            13, 16,
+                            13, 16
+                    );
+                }
+
+                context.drawString(font, String.valueOf((float) PetrificationCooldown1), x + 20, y + 14, 0xFFFFFFFF, true);
+                context.drawString(font, String.valueOf((float) PetrificationCooldown2), x + 54, y + 14, 0xFFFFFFFF, true);
+                context.drawString(font, String.valueOf((float) PetrificationCooldown3), x + 87, y + 14, 0xFFFFFFFF, true);
             }
 
         }

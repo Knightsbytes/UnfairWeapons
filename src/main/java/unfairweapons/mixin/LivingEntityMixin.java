882002bc7760ec61aad8127e2884d758b977ab66
ben <benjaminburton2020@outlook.com>
2026-01-25 18:43:20 +0000

    More mixin

--- src/main/java/unfairweapons/mixin/LivingEntityMixin.java
+++ src/main/java/unfairweapons/mixin/LivingEntityMixin.java
@@ -1,14 +1,12 @@
 package unfairweapons.mixin;
 
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import static unfairweapons.UnfairWeapons.PETRIFICATION_EFFECT;
 
@@ -21,18 +19,19 @@ public abstract class LivingEntityMixin {
             argsOnly = true,
             ordinal = 1
     )
-    private void modifyDamageWithEffect(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
+    private float modifyDamageAmount(float amount, ServerLevel level, DamageSource source) {
         LivingEntity entity = (LivingEntity) (Object) this;
 
         if (entity.hasEffect(PETRIFICATION_EFFECT)) {
             MobEffectInstance effectInstance = entity.getEffect(PETRIFICATION_EFFECT);
             int amplifier = effectInstance.getAmplifier();
 
-            float modifiedAmount = amount * (1.0f - (0.2f * (amplifier + 1)));
+            amount = amount * (1.0f - (0.2f * (amplifier + 1)));
 
-            if (amount > 5){
+            if (amount > 5) {
                 amount = 5;
             }
         }
+        return amount;
     }
 }

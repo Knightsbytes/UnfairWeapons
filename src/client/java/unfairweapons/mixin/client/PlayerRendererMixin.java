c0dbba54c50506731bc06cb307ab796b7fd9a386
ben <benjaminburton2020@outlook.com>
2026-02-04 16:39:55 +0000

    working on the horns

--- src/client/java/unfairweapons/mixin/client/PlayerRendererMixin.java
+++ src/client/java/unfairweapons/mixin/client/PlayerRendererMixin.java
@@ -3,9 +3,7 @@ package unfairweapons.mixin.client;
 import net.minecraft.client.model.geom.EntityModelSet;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.LivingEntityRenderer;
-import net.minecraft.client.renderer.entity.layers.RenderLayer;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -14,32 +12,24 @@ import unfairweapons.ModelLayers;
 import unfairweapons.models.StableEldritchHorns;
 
 @Mixin(net.minecraft.client.renderer.entity.player.AvatarRenderer.class)
-public abstract class PlayerRendererMixin {
+public abstract class PlayerRendererMixin extends LivingEntityRenderer {
 
-    @Shadow
-    protected abstract boolean addLayer(RenderLayer<?, ?> layer);
+    // Mixin requires this constructor even though it won't be called
+    public PlayerRendererMixin(EntityRendererProvider.Context context, net.minecraft.client.model.EntityModel model, float shadowRadius) {
+        super(context, model, shadowRadius);
+    }
 
-    @Inject(
-            method = "<init>",
-            at = @At("TAIL")
-    )
+    @Inject(method = "<init>", at = @At("TAIL"))
     private void unfairweapons$addHorns(
             EntityRendererProvider.Context context,
             boolean slim,
             CallbackInfo ci
     ) {
         System.out.println("ADDING HORNS TO PLAYER");
-
         EntityModelSet modelSet = context.getModelSet();
-
-        StableEldritchHorns horns =
-                new StableEldritchHorns(
-                        modelSet.bakeLayer(ModelLayers.CUSTOM_HORNS)
-                );
-
-        this.addLayer(new FeatureRenderer(
-                (LivingEntityRenderer)(Object)this,
-                horns
-        ));
+        StableEldritchHorns horns = new StableEldritchHorns(
+                modelSet.bakeLayer(ModelLayers.CUSTOM_HORNS)
+        );
+        this.addLayer(new FeatureRenderer(this, horns));
     }
 }

017b284b42836d941a1c784537fb970c1345e23d
ben <benjaminburton2020@outlook.com>
2026-02-18 21:15:03 +0000

    changes for crosshair

--- src/client/java/unfairweapons/mixin/client/DisorientationGuiMixin.java
+++ src/client/java/unfairweapons/mixin/client/DisorientationGuiMixin.java
@@ -2,11 +2,14 @@ package unfairweapons.mixin.client;
 
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Ordering;
+import net.minecraft.client.AttackIndicatorStatus;
 import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.Options;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.*;
+import net.minecraft.client.gui.components.debug.DebugScreenEntries;
 import net.minecraft.client.gui.components.spectator.SpectatorGui;
 import net.minecraft.client.gui.contextualbar.ContextualBarRenderer;
 import net.minecraft.client.gui.contextualbar.ExperienceBarRenderer;
@@ -14,12 +17,16 @@ import net.minecraft.client.gui.contextualbar.JumpableVehicleBarRenderer;
 import net.minecraft.client.gui.contextualbar.LocatorBarRenderer;
 import net.minecraft.client.renderer.RenderPipelines;
 import net.minecraft.core.Holder;
+import net.minecraft.core.component.DataComponents;
 import net.minecraft.resources.Identifier;
 import net.minecraft.util.ARGB;
 import net.minecraft.util.Mth;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.component.AttackRange;
+import net.minecraft.world.level.GameType;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -38,6 +45,10 @@ public abstract class DisorientationGuiMixin {
     private static final Identifier EFFECT_BACKGROUND_AMBIENT_SPRITE = Identifier.withDefaultNamespace("hud/effect_background_ambient");
     private static final Identifier EFFECT_BACKGROUND_SPRITE = Identifier.withDefaultNamespace("hud/effect_background");
     private static final Identifier EFFECT_DISORIENTED_SPRITE = Identifier.fromNamespaceAndPath(MOD_ID, "mob_effect/disorientation");
+    private static final Identifier CROSSHAIR_SPRITE = Identifier.withDefaultNamespace("hud/crosshair");
+    private static final Identifier CROSSHAIR_ATTACK_INDICATOR_FULL_SPRITE = Identifier.withDefaultNamespace("hud/crosshair_attack_indicator_full");
+    private static final Identifier CROSSHAIR_ATTACK_INDICATOR_BACKGROUND_SPRITE = Identifier.withDefaultNamespace("hud/crosshair_attack_indicator_background");
+    private static final Identifier CROSSHAIR_ATTACK_INDICATOR_PROGRESS_SPRITE = Identifier.withDefaultNamespace("hud/crosshair_attack_indicator_progress");
 
     public DisorientationGuiMixin(Minecraft minecraft) {
         this.minecraft = minecraft;
@@ -56,7 +67,7 @@ public abstract class DisorientationGuiMixin {
 
     @Inject(
             method = "renderEffects",
-            at = @At("HEAD"),
+            at = @At("RETURN"),
             cancellable = true
     )
     private void renderEffects(GuiGraphics guiGraphics, DeltaTracker deltaTracker, CallbackInfo ci) {
@@ -100,6 +111,51 @@ public abstract class DisorientationGuiMixin {
                     }
                     else{
                         guiGraphics.blitSprite(RenderPipelines.GUI_TEXTURED, getMobEffectSprite(holder), k + 3, l + 3, 18, 18, ARGB.white(f));
+                    }
+                }
+            }
+        }
+    }
+
+    @Inject(
+            method = "renderCrosshair",
+            at = @At("HEAD"),
+            cancellable = true
+    )
+    private void renderCrosshair(GuiGraphics guiGraphics, DeltaTracker deltaTracker, CallbackInfo ci) {
+
+        Options options = this.minecraft.options;
+        if (options.getCameraType().isFirstPerson()) {
+            if (this.minecraft.gameMode.getPlayerMode() != GameType.SPECTATOR) {
+                if (!this.minecraft.debugEntries.isCurrentlyEnabled(DebugScreenEntries.THREE_DIMENSIONAL_CROSSHAIR)) {
+                    guiGraphics.nextStratum();
+                    int i = 15;
+                    if (this.minecraft.player.hasEffect(INCAPACITATION_EFFECT)){
+                        guiGraphics.blitSprite(RenderPipelines.CROSSHAIR, CROSSHAIR_SPRITE, (guiGraphics.guiWidth() - 55) / 2, (guiGraphics.guiHeight() - 15) / 2, 15, 15);
+                    }
+                    else{
+                        guiGraphics.blitSprite(RenderPipelines.CROSSHAIR, CROSSHAIR_SPRITE, (guiGraphics.guiWidth() - 15) / 2, (guiGraphics.guiHeight() - 15) / 2, 15, 15);
+                    }
+                    if (this.minecraft.options.attackIndicator().get() == AttackIndicatorStatus.CROSSHAIR) {
+                        float f = this.minecraft.player.getAttackStrengthScale(0.0F);
+                        boolean bl = false;
+                        if (this.minecraft.crosshairPickEntity != null && this.minecraft.crosshairPickEntity instanceof LivingEntity && f >= 1.0F) {
+                            bl = this.minecraft.player.getCurrentItemAttackStrengthDelay() > 5.0F;
+                            bl &= this.minecraft.crosshairPickEntity.isAlive();
+                            AttackRange attackRange = (AttackRange)this.minecraft.player.getActiveItem().get(DataComponents.ATTACK_RANGE);
+                            bl &= attackRange == null || attackRange.isInRange(this.minecraft.player, this.minecraft.hitResult.getLocation());
+                        }
+
+                        int j = guiGraphics.guiHeight() / 2 - 7 + 16;
+                        int k = guiGraphics.guiWidth() / 2 - 8;
+                        if (bl) {
+                            guiGraphics.blitSprite(RenderPipelines.CROSSHAIR, CROSSHAIR_ATTACK_INDICATOR_FULL_SPRITE, k, j, 16, 16);
+                        } else if (f < 1.0F) {
+                            int l = (int)(f * 17.0F);
+                            guiGraphics.blitSprite(RenderPipelines.CROSSHAIR, CROSSHAIR_ATTACK_INDICATOR_BACKGROUND_SPRITE, k, j, 16, 4);
+                            guiGraphics.blitSprite(RenderPipelines.CROSSHAIR, CROSSHAIR_ATTACK_INDICATOR_PROGRESS_SPRITE, 16, 4, 0, 0, k, j, l, 4);
+                        }
+                        ci.cancel();
                     }
                 }
             }

7a69e4c04bfdc44b0629a7a15ca55514a588eec7
ben <benjaminburton2020@outlook.com>
2026-03-04 23:34:34 +0000

    Other

---
+++ src/client/java/unfairweapons/mixin/client/ItemStackMixin.java
@@ -0,0 +1,13 @@
+package unfairweapons.mixin.client;
+
+import net.minecraft.core.component.PatchedDataComponentMap;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.ItemLike;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+
+@Mixin(ItemStack.class)
+public class ItemStackMixin{
+
+}
